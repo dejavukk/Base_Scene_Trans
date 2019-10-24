7aa6d1b8ea5aca7466323174be4565ce7fe0bd30
junhyuk <dejavukk1@gmail.com>
2019-10-24 22:48:35 +0900

    Initial Commit

---
+++ Base_Scene_Trans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Base_Scene_Trans
+//
+//  Created by JunHyuk on 2019/10/24.
+//  Copyright © 2019 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
