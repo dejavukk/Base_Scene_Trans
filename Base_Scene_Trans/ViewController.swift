a0aa62f5f7b0c58ed3fc777e66817b5e12dc1bcc
junhyuk <dejavukk1@gmail.com>
2019-10-28 23:42:22 +0900

    4개의 화면이동 Segueway구현.

--- Base_Scene_Trans/ViewController.swift
+++ Base_Scene_Trans/ViewController.swift
@@ -16,32 +16,10 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func moveByNavy(_ sender: UIBarButtonItem) {
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
         
-        // 두번째 뷰 컨트롤러의 인스턴스를 가져온다.
-        guard let uvc = self.storyboard?.instantiateViewController(identifier: "SecondVC") else {
-            return
-        }
+        print("홈으로 이동.")
         
-        // 화면 전환. pushViewController - 메소드의 호출대상은 내비게이션 컨트롤러.
-        self.navigationController?.pushViewController(uvc, animated: true)
-    }
-    
-    @IBAction func movePresent(_ sender: UIButton) {
-        
-        guard let uvc = self.storyboard?.instantiateViewController(identifier: "SecondVC") else {
-            return
-        }
-        
-        // 화면 전환. present - 메소드의 호출 대상은 뷰컨트롤러 자신.
-        self.present(uvc, animated: true)
-    }
-    
-    
-    @IBAction func wind(_ sender: UIButton) {
-        
-        // 세그웨이를 실행한다.
-        self.performSegue(withIdentifier: "ManualWind", sender: self)
         
     }
     
